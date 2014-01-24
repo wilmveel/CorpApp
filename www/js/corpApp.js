d1743588608d1c4220ca5ee3c6ad4a32e8aa27bb
Willem Veelenturf <willem.veelenturf@gmail.com>
2014-01-24 14:31:49 +0100

    Look and feel

--- www/js/corpApp.js
+++ www/js/corpApp.js
@@ -13,12 +13,24 @@ corpApp.config(['$routeProvider',
       });
   }]);
   
-	corpApp.controller('HomeController', function($scope, $http) {
+corpApp.controller('HomeController', function($scope, $http) {
 	
-		$scope.test = "test";
+	$scope.test = "test";
+	
+	$scope.color = function(i){
+	
+		color = {
+			"0" : "regular",
+			"1" : "medium",
+			"2" : "dark",
+			"3" : "light",
+		};
+				
+		return color[i % 4];
+	};
 
-		$scope.modules = ["PeopleFinder", "expenses", "buddy", "departments"];
-	});
+	$scope.modules = ["PeopleFinder", "expenses", "buddy", "departments"];
+});
 corpApp.controller('ModuleController', function($scope, $http) {
 	
 	$scope.test = "test";
