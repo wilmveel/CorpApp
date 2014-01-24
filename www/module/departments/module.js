b08957ea7ed3bc7e8e27f8248edc6b7b74d3db0f
chaukema <chrisaukema@gmail.com>
2014-01-24 14:33:15 +0100

    department module

--- www/module/departments/module.js
+++ www/module/departments/module.js
@@ -7,12 +7,22 @@ contactModule.config(['$routeProvider',
 		when('/module/departments', {
 			templateUrl: 'module/departments/list.html',
 			controller: 'DepartmentsController'
-		})
+		});
 	}
 ]);
 
 contactModule.controller('DepartmentsController', function($scope, $http) {
 
-	$scope.list = ["Erik Mons", "Chris Aukema"];
+$http({method: 'GET', url: 'http://192.168.101.192:8080/departments/'}).
+        success(function(data, status, headers, config) {
+            // this callback will be called asynchronously
+            // when the response is available
+            $scope.list = data;
+        }).
+        error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            $scope.list = ["Error"];
+        });
  
 });
