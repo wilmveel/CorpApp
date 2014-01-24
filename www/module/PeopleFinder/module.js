f9d0456ba4831f8b841f36a63e1bbd4b823852fa
Saman <saman.sattari@gmail.com>
2014-01-24 20:42:30 +0100

    added back functionality

--- www/module/PeopleFinder/module.js
+++ www/module/PeopleFinder/module.js
@@ -7,25 +7,27 @@ contactModule.config(['$routeProvider',
             when('/module/PeopleFinder', {
             templateUrl: 'module/PeopleFinder/list.html',
             controller: 'PeopleFinderController'
-        })
+        }). when('/module/PeopleFinder/Details/:contactId', {
+                templateUrl: 'module/PeopleFinder/details.html',
+                controller: 'PeopleDetailController'
+            })
     }
 ]);
 
-contactModule.controller('PeopleFinderController', function ($scope, $http) {
+contactModule.controller('PeopleFinderController', function ($rootScope, $scope, $http, $location, $routeParams, PeopleDetailservice) {
+
+    $scope.list = PeopleDetailservice.getList();
 
     $scope.search = function () {
 
-        $http({method: 'GET', url: 'http://192.168.101.192:8080/contact/' + $scope.searchText}).
-            success(function (data, status, headers, config) {
-                // this callback will be called asynchronously
-                // when the response is available
-                $scope.list = data;
-            }).
-            error(function (data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                $scope.list = ["Error"];
-            });
+        var promise = PeopleDetailservice.search($scope.searchText);
+        promise.then(function(value) {
+            $scope.list = value;
+        }, function(reason) {
+            $scope.list = [];
+        }, function(update) {
+            $scope.list = [];
+        });
 
     };
 
@@ -37,7 +39,77 @@ contactModule.controller('PeopleFinderController', function ($scope, $http) {
         }
     };
 
-    $scope.go = function ( path ) {
-        $location.path( path );
+});
+
+contactModule.controller('PeopleDetailController', function ($rootScope, $scope, $http, $location, $routeParams, PeopleDetailservice) {
+    var init = function () {
+
+        var promise = PeopleDetailservice.getDetails($routeParams.contactId);
+        promise.then(function(value) {
+            $scope.detail = value;
+        }, function(reason) {
+            $scope.detail = {};
+        }, function(update) {
+            $scope.detail = {};
+        });
+
     };
+
+    init();
+});
+
+contactModule.service('PeopleDetailservice', function ($http, $q) {
+
+    var list = [];
+
+    this.getList = function(){
+        return list;
+    };
+
+    this.search = function(searchText){
+        var deferred = $q.defer();
+
+        setTimeout(function() {
+            // since this fn executes async in a future turn of the event loop, we need to wrap
+            // our code into an $apply call so that the model changes are properly observed.
+            $http({method: 'GET', url: 'http://192.168.101.192:8080/searchContact/' + searchText}).
+                success(function (data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    list = data;
+                    deferred.resolve(data);
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    list = [];
+                    deferred.resolve(list);
+                });
+        }, 5);
+
+        return deferred.promise;
+    };
+
+    this.getDetails = function(contactId){
+        var deferred = $q.defer();
+
+        setTimeout(function() {
+            // since this fn executes async in a future turn of the event loop, we need to wrap
+            // our code into an $apply call so that the model changes are properly observed.
+            $http({method: 'GET', url: 'http://192.168.101.192:8080/contact/' + contactId}).
+                success(function (data, status, headers, config) {
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    deferred.resolve(data);
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    deferred.resolve({});
+                });
+        }, 5);
+
+        return deferred.promise;
+    };
+
 });
