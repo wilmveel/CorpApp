ddda6f467cb5ecb69cb5a9c7ff40c34a45da466b
Rui Chen <ruiruichen@gmail.com>
2014-01-25 15:20:24 +0100

    improve code

--- www/module/profile/module.js
+++ www/module/profile/module.js
@@ -15,7 +15,8 @@ profileModule.controller('ProfileController', function($scope, $http, Loginservi
 			$scope.status = {};
 			$scope.available = "";
 			$scope.notavailable = "";
-			//$scope.list = [];
+
+            $scope.item = {};
 			
 			//$scope.list = $http.get("http://192.168.101.192:8080/searchcontact/Alejandro");
 				
@@ -23,39 +24,37 @@ profileModule.controller('ProfileController', function($scope, $http, Loginservi
 	        success(function(data, status, headers, config) {
 	            // this callback will be called asynchronously
 	            // when the response is available
-	            $scope.list = data;
+	            $scope.item = data[0];
 	    
 	        }).
 	        error(function(data, status, headers, config) {
 	            // called asynchronously if an error occurs
 	            // or server returns response with an error status.
-	            $scope.list = ["Error"];
+	            $scope.list = {};
+                    alert("Error");
 	        });
 
-$scope.edit = function(){
-		console.log("Edit profile: ", $scope.list[0]);
+    $scope.edit = function(){
+		console.log("Edit profile: ", $scope.item);
 		//var url = "http://192.168.101.192:8080/contact";
 		//console.log(url);
-		$http({method: 'POST', url: 'http://192.168.101.192:8080/contact', data: $scope.list[0]}).
-		success(function(data, status){
-				console.log(data);
-				$scope.status = true;
-			}).error(function(data,status,headers,config){
-				console.log(data);
-				console.log(status);
-				console.log(headers);
-				console.log(config);
-				$scope.status = false;
-			});
-	};
-	
-	
-	
-	
 
-			
-			
+        console.log("Form data", $scope.item);
+
+		$http({method: 'POST', url: 'http://192.168.101.192:8080/contact', data: $scope.item}).
+		success(function(data, status){
+            console.log(data);
+            $scope.status = true;
+        }).error(function(data,status,headers,config){
+            console.log(data);
+            console.log(status);
+            console.log(headers);
+            console.log(config);
+            $scope.status = false;
         });
+	};
+			
+});
 
 
 
