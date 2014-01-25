72eca76fad8fe43ed9e4c25360bbc710c60d925b
Willem Veelenturf <willem.veelenturf@gmail.com>
2014-01-25 13:44:34 +0100

    Update expenses module

--- www/module/expenses/module.js
+++ www/module/expenses/module.js
@@ -7,6 +7,10 @@ expensesModule.config(['$routeProvider',
 		when('/module/expenses', {
 			templateUrl: 'module/expenses/camera.html',
 			controller: 'ExpensesController'
+		}).
+		when('/module/expenses/form', {
+			templateUrl: 'module/expenses/form.html',
+			controller: 'ExpensesController'
 		})
 	}
 ]);
@@ -15,6 +19,10 @@ expensesModule.controller('ExpensesController', function($scope) {
 
 $scope.camera = function(){		
 	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50, destinationType: destinationType.DATA_URL });
+}
+
+$scope.gallery = function(){		
+	navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, destinationType: destinationType.FILE_URI, sourceType: source });
 }
 
 $scope.save = function(){		
@@ -26,86 +34,86 @@ $scope.save = function(){
 
 
 var pictureSource;   // picture source
-    var destinationType; // sets the format of returned value
+var destinationType; // sets the format of returned value
 
-    // Wait for device API libraries to load
-    //
-    document.addEventListener("deviceready",onDeviceReady,false);
+// Wait for device API libraries to load
+//
+document.addEventListener("deviceready",onDeviceReady,false);
 
-    // device APIs are available
-    //
-    function onDeviceReady() {
-        pictureSource=navigator.camera.PictureSourceType;
-        destinationType=navigator.camera.DestinationType;
-    }
+// device APIs are available
+//
+function onDeviceReady() {
+	pictureSource=navigator.camera.PictureSourceType;
+	destinationType=navigator.camera.DestinationType;
+}
 
-    // Called when a photo is successfully retrieved
-    //
-    function onPhotoDataSuccess(imageData) {
-      // Uncomment to view the base64-encoded image data
-      // console.log(imageData);
+// Called when a photo is successfully retrieved
+//
+function onPhotoDataSuccess(imageData) {
+  // Uncomment to view the base64-encoded image data
+  // console.log(imageData);
 
-      // Get image handle
-      //
-      var smallImage = document.getElementById('smallImage');
+  // Get image handle
+  //
+  var smallImage = document.getElementById('smallImage');
 
-      // Unhide image elements
-      //
-      smallImage.style.display = 'block';
+  // Unhide image elements
+  //
+  smallImage.style.display = 'block';
 
-      // Show the captured photo
-      // The in-line CSS rules are used to resize the image
-      //
-      smallImage.src = "data:image/jpeg;base64," + imageData;
-    }
+  // Show the captured photo
+  // The in-line CSS rules are used to resize the image
+  //
+  smallImage.src = "data:image/jpeg;base64," + imageData;
+  
+  window.location.href = "#/module/expenses/form";
+}
 
-    // Called when a photo is successfully retrieved
-    //
-    function onPhotoURISuccess(imageURI) {
-      // Uncomment to view the image file URI
-      // console.log(imageURI);
+// Called when a photo is successfully retrieved
+//
+function onPhotoURISuccess(imageURI) {
+	// Uncomment to view the image file URI
+	// console.log(imageURI);
 
-      // Get image handle
-      //
-      var largeImage = document.getElementById('largeImage');
+	// Get image handle
+	//
+	var largeImage = document.getElementById('largeImage');
 
-      // Unhide image elements
-      //
-      largeImage.style.display = 'block';
+	// Unhide image elements
+	//
+	largeImage.style.display = 'block';
 
-      // Show the captured photo
-      // The in-line CSS rules are used to resize the image
-      //
-      largeImage.src = imageURI;
-    }
+	// Show the captured photo
+	// The in-line CSS rules are used to resize the image
+	//
+	largeImage.src = imageURI;
+	
+	window.location.href = "#/module/expenses/form";
+}
 
-    // A button will call this function
-    //
-    function capturePhoto() {
-      // Take picture using device camera and retrieve image as base64-encoded string
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-        destinationType: destinationType.DATA_URL });
-    }
+// A button will call this function
+//
+function capturePhoto() {
+	// Take picture using device camera and retrieve image as base64-encoded string
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50, destinationType: destinationType.DATA_URL });
+}
 
-    // A button will call this function
-    //
-    function capturePhotoEdit() {
-      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
-        destinationType: destinationType.DATA_URL });
-    }
+// A button will call this function
+//
+function capturePhotoEdit() {
+	// Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true, destinationType: destinationType.DATA_URL });
+}
 
-    // A button will call this function
-    //
-    function getPhoto(source) {
-      // Retrieve image file location from specified source
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-        destinationType: destinationType.FILE_URI,
-        sourceType: source });
-    }
+// A button will call this function
+//
+function getPhoto(source) {
+	// Retrieve image file location from specified source
+	navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, destinationType: destinationType.FILE_URI, sourceType: source });
+}
 
-    // Called if something bad happens.
-    //
-    function onFail(message) {
-      alert('Failed because: ' + message);
-    }
+// Called if something bad happens.
+//
+function onFail(message) {
+	alert('Failed because: ' + message);
+}
