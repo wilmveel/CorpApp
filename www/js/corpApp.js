31487303e18db56a6c86fffd1550fe25c7166954
wouterslager <wouter.slager@capgemini.com>
2014-01-25 15:16:38 +0100

    Added themeswitcher to logo
    Switch between Capgemini and ING theme by clicking on logo

--- www/js/corpApp.js
+++ www/js/corpApp.js
@@ -59,3 +59,20 @@ corpApp.controller('ModuleController', function($scope, $http) {
 	$scope.test = "test";
 	 
 });
+
+
+
+corpApp.controller('mainCtrl', function($scope) {
+    $scope.changeTheme = function() {
+      var linkEls = document.querySelectorAll('link.theme');
+      var sheetIndex = 0;
+      angular.forEach(linkEls, function(stylesheet, i) {
+        if (!stylesheet.disabled) {
+          sheetIndex = i;
+        }
+      });
+      linkEls[sheetIndex].disabled = true;
+      linkEls[(sheetIndex + 1) % linkEls.length].disabled = false;
+      sessionStorage['theme'] = linkEls[(sheetIndex + 1) % linkEls.length].href;
+    };
+});
