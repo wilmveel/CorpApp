668d49eca423b00812ed64f776cd5584d9cf273b
Bart Altena <bart.altena@capgemini.com>
2014-02-24 21:33:13 +0100

    Implemented: CAP-33

--- www/module/carpool/module.js
+++ www/module/carpool/module.js
@@ -11,6 +11,43 @@ carpoolModule.config(['$routeProvider',
     }
 ]);
 
-carpoolModule.controller('CarpoolController', function($scope, $http) {
+//filters
+carpoolModule.filter('carpoolFilter', function() {
+  return function(input, location) {
+    var out = [];
+      for (var i = 0; i < input.length; i++){
+          if(input[i].from == location.from){
+              out.push(input[i]);
+          }
+
+          if(input[i].to == location.to){
+              out.push(input[i]);
+           }
+      }      
+    return out;
+  };
+});
+
+carpoolModule.controller('CarpoolController', function($scope, $http, $filter, config) {
+
+	$scope.list = [];
+
+
+	$scope.searchRide = function(){
+		$http.get(config.API_URL + '/serviceCarpool').
+		success(function(data, status, headers, config) {
+			$scope.list = data;
+			$scope.list = $filter('carpoolFilter')($scope.list, {from : $scope.fromLocation, to : $scope.toLocation});
+		}).
+		error(function(data, status, headers, config) {
+			//$location.path("/");
+		});
+		
+	};
+
+	$scope.submitRide = function(){
+		alert("From: "+$scope.fromLocation+"<br />To:"+$scope.toLocation);
+	};
+
  
 });
